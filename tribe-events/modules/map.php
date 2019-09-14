9a353c497053c5b1b0cb98491d4c419b77f8e68b
hagege <webmaster@aachenerkinder.de>
2019-09-14 07:06:06 +0200

    Regel für Tabelle auf Untersützerseite; Test leaflet-map (unvollständig)

---
+++ tribe-events/modules/map.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template used for maps embedded within single events and venues.
+ * Override this template in your own theme by creating a file at:
+ *
+ *     [your-theme]/tribe-events/modules/map.php
+ *
+ * @version 4.6.19
+ *
+ * @var $index
+ * @var $width
+ * @var $height
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+$address = tribe_get_address( $venue_id ).", ".tribe_get_zip( $venue_id )." ".tribe_get_city( $venue_id ).", ".tribe_get_country( $venue_id );
+$shortcode  = '[leaflet-map address="'.$address.'" zoom="15" fit_markers="1"]';
+echo do_shortcode($shortcode);
